ad4258cdf2f24e62bf843a6ac3a1d5cf29c98ef4
Andon Mancev <andonman@gmail.com>
2014-04-14 12:35:01 +0200

    Added header comments

--- cachematrix.R
+++ cachematrix.R
@@ -11,10 +11,25 @@ if (!is.element('MASS', installed.packages()[,1]))
 	library(MASS)
 
 
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object that stores
+## a numeric matrix and cache it's inverse
 
-## Write a short comment describing this function
+# For example the following code sets matrix c,
+# c=rbind(c(1, -1/4), c(-1/4, 1)) 
+# And x<-  makeCacheMatrix(c) to init the object list
+# After this, the call to solve the matrix, gives 
+# the inverse of the original c matrix
+# cacheSolve(x) , calculates the inverse matrix of c
+# If it is called again, inverse is get from the cache
+
+
+## The first function, makeCacheMatrix creates a list containing a function to
+## - set the values from input matrix
+## - get the values of currently stored matrix
+## - set the values of the inverse matrix
+## - get the values of the inverse matrix
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
