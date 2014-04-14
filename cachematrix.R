bef8420d5ee5cd723f07217d9af2e2b3e63dbaa0
Andon Mancev <andonman@gmail.com>
2014-04-14 12:31:50 +0200

    Added check if MASS package is installed

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,16 @@
+i## First check if MASS package is installed
+
+if (!is.element('MASS', installed.packages()[,1])) 
+{
+    install.packages('MASS')
+    }
+    else
+    {
+        print ("MASS package already installed")
+	}
+	library(MASS)
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
